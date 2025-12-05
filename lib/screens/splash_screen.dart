9c4b04ef8cc14a4dd2a38c683f92640637f7dce6
KIRAN10077 <kasalawotkiran@gmail.com>
2025-12-05 12:47:59 +0545

    splash screen setup

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(137, 255, 255, 255),
+      body: Container(
+        decoration: const BoxDecoration(
+         gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFFE1FBFF), // light aqua
+                Color(0xFFCFFFE1), // light green
+      ],
+    ),
+  ),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 250,),
+              Image.asset('assets/images/matchpoint_logo_final.png',height:300,),
+              Text("MatchPoint", 
+              style: GoogleFonts.audiowide(fontSize: 50, color: Colors.black),
+          ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
