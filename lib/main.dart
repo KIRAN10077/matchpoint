cb20991b08d7afac64b5277f30a3b5cf237c8f42
KIRAN10077 <kasalawotkiran@gmail.com>
2026-01-02 13:57:06 +0545

    extensions and tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:matchpoint/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:matchpoint/app/app.dart';
+import 'package:matchpoint/core/services/hive/hive_service.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Hive
+  final hiveService = HiveService();
+  await hiveService.init();
+  await hiveService.openBoxes();
+  
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
-
