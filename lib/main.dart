281b6af9050ca1e527f4d4ce6ecd07eb9a6fa7f1
KIRAN10077 <kasalawotkiran@gmail.com>
2026-03-04 22:07:09 +0545

    main wiring refresh

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:matchpoint/app/app.dart';
 import 'package:matchpoint/core/services/hive/hive_service.dart';
 import 'package:matchpoint/core/services/storage/user_session_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (WebViewPlatform.instance == null) {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        WebViewPlatform.instance = AndroidWebViewPlatform();
+        break;
+      case TargetPlatform.iOS:
+        WebViewPlatform.instance = WebKitWebViewPlatform();
+        break;
+      default:
+        break;
+    }
+  }
 
   try {
     final hiveService = HiveService();
